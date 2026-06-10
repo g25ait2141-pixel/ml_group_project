e8ea71f2de0f78c2426a3cb41f468393b43a4426
sai <g25ait2141@iitj.ac.in>
2026-06-10 23:32:57 +0530

    Create inference.py
    This python files is used to pull the model from hugging face and run in the local machine using the docker hub

---
+++ src/inference.py
@@ -0,0 +1,47 @@
+import os
+import wandb
+from transformers import pipeline
+
+MODEL_ID = "mehtayash12345678/mlops-ag_news_classification-distilbert"
+
+WANDB_PROJECT = "mlops-ag_news_classification-distilbert"
+WANDB_ENTITY = "g25ait2133-indian-institute-technology-jodhpur"
+
+
+def main():
+    input_text = os.environ.get("INPUT_TEXT")
+    if not input_text:
+        raise ValueError("INPUT_TEXT environment variable is not set.")
+
+    hf_token = os.environ.get("HF_TOKEN")
+        
+  
+    """classifier = pipeline(
+        "text-classification",
+        model=MODEL_ID,
+        token=hf_token,
+    )"""
+
+    result = classifier(input_text)[0]
+    label_name = result["label"]
+    confidence = result["score"]
+
+    print("=" * 50)
+    print(f"Input: {input_text}")
+    print(f"Predicted Label: {label_name}")
+    print(f"Confidence: {confidence:.4f}")
+    print("=" * 50)
+
+    wandb.log(
+        {
+            "input_text": input_text,
+            "predicted_label": label_name,
+            "confidence": confidence,
+        }
+    )
+
+    wandb.finish()
+
+
+if __name__ == "__main__":
+    main()
